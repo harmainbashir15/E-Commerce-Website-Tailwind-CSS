18fe1b4971b3794380ecb863eb54ef721d371a5a
harmainbashir15 <harmainfatima127@gmail.com>
2024-11-09 20:31:57 +0500

    first commit

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -9,7 +9,7 @@ function About() {
       <div className="lg:w-1/2 w-full mb-6  lg:mb-0"> 
         <h1 className="sm:text-4xl text-2xl g-3 font-bold mb-2 text-black" data-aos="zoom-in-up"> Our Specialities</h1>
       </div>
-      <p className="lg:w-10/12 w-full text-bold text-black" data-aos="zoom-in-up">"At <b>The Cake Palette</b>, our journey began with a passion for baking and a dream to share the warmth and comfort of freshly made goods with our community. From humble beginnings in a small kitchen to a beloved neighborhood bakery, we’ve grown thanks to the love and support of our loyal customers. Every day, we bake with heart, using the finest ingredients to create delicious treats that bring people together. Join us in celebrating the art of baking and the simple joys of life, one bite at a time."</p>
+      <p className="lg:w-10/12 w-full text-bold text-black" data-aos="zoom-in-up">"At <b>The Cake Palette</b>, our journey began with a passion for baking and a dream to share the warmth and comfort of freshly made goods with our community. From humble beginnings in a small kitchen to a beloved neighborhood bakery, we have grown thanks to the love and support of our loyal customers. Every day, we bake with heart, using the finest ingredients to create delicious treats that bring people together. Join us in celebrating the art of baking and the simple joys of life, one bite at a time."</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/4 md:w-1/2 p-4">
@@ -38,7 +38,7 @@ function About() {
         <div className="p-6 rounded-lg transition-transform duration-200 ease-in-out transform hover:scale-110 ">
           <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/cake.jpg" alt="image" width={400} height={300}/> 
           <h2 className="text-lg text-black font-bold mb-4">Cakes </h2>
-          <p className="text-base">  And let’s not forget our cakes, which are beautifully designed and bursting with flavor, making them the perfect centerpiece for any celebration. Whether it’s a classic vanilla, rich chocolate, or a unique seasonal creation, our cakes are made to elevate every special occasion. Every item we create is made with love and the finest ingredients, ensuring that each bite is a moment to savor.
+          <p className="text-base">  And let  not forget our cakes, which are beautifully designed and bursting with flavor, making them the perfect centerpiece for any celebration. Whether it  a classic vanilla, rich chocolate, or a unique seasonal creation, our cakes are made to elevate every special occasion. Every item we create is made with love and the finest ingredients, ensuring that each bite is a moment to savor.
              We invite you to visit our bakery and experience the sweet magic for yourself!</p>
         </div>
       </div>
